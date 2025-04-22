a7e9b8e00e14c9956b7d5001b97d5d4156dc4795
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2025-04-22 20:12:52 +0700

    chore: changes message

--- src/messages/messages.ts
+++ src/messages/messages.ts
@@ -1,18 +1,18 @@
 export const bloodMessages = {
     first: [
-      "🩸 *FIRST BLOOD!* 🩸\n🥇 *{user}* from team *{team}* was the first to conquer *{challenge}*! 💥",
-      "⚔️ *FIRST STRIKE!* ⚔️\n🥇 *{user}* of *{team}* cracked *{challenge}* before anyone else!",
-      "🚨 *FIRST SOLVE ALERT!* 🚨\n🥇 Challenge *{challenge}* has been defeated first by *{user}* of *{team}*!"
+      "🩸 *FIRST BLOOD!* 🩸\n🥇 Team *{team}* was the first to conquer *{challenge}*! 💥",
+      "⚔️ *FIRST STRIKE!* ⚔️\n🥇 *{team}* cracked *{challenge}* before anyone else!",
+      "🚨 *FIRST SOLVE ALERT!* 🚨\n🥇 Challenge *{challenge}* has been defeated first by *{team}*!"
     ],
     second: [
-      "🩸 *SECOND BLOOD!* 🩸\n🥈 *{user}* from team *{team}* followed closely behind on *{challenge}*!",
-      "🩸 *SECOND BLOOD!* 🩸\n🥈 *{user}* of *{team}* grabbed the *second solve* for *{challenge}*! ⚡",
-      "🩸 *SECOND BLOOD!* 🩸\n🥈 *{user}* (Team: *{team}*) secured second place on *{challenge}*!"
+      "🥈 *SECOND BLOOD!* 🩸\n🥈 Team *{team}* followed closely behind on *{challenge}*!",
+      "🩸 *SECOND BLOOD!* 🩸\n🥈 *{team}* grabbed the *second solve* for *{challenge}*! ⚡",
+      "🩸 *SECOND BLOOD!* 🩸\n🥈 Team: *{team}* secured second place on *{challenge}*!"
     ],
     third: [
-      "🩸 *THIRD BLOOD!* 🩸\n🥉 *{user}* from *{team}* took the third spot for *{challenge}*!",
-      "🩸 *THIRD BLOOD!* 🩸\n🥉 *{user}* of *{team}* lands third on *{challenge}* 🧠",
-      "🩸 *THIRD BLOOD!* 🩸\n🥉 *{user}* (Team: *{team}*) claimed the third solve on *{challenge}*!"
+      "🥉 *THIRD BLOOD!* 🩸\n🥉 *{team}* took the third spot for *{challenge}*!",
+      "🥉 *THIRD BLOOD!* 🩸\n🥉 *{team}* lands third on *{challenge}* 🧠",
+      "🥉 *THIRD BLOOD!* 🩸\n🥉 Team *{team}* claimed the third solve on *{challenge}*!"
     ]
   } as const;
 
